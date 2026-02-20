bc0b5f3d7757358246861fcd1e6d195c90de8c36
jgomezz <jgomezz@gmail.com>
2026-02-19 20:25:43 -0500

    update

---
+++ sesion_01_ejercicios/06_ejercicio_input.py
@@ -0,0 +1,23 @@
+'''
+Ingresar 2 numeros por teclado, sumarlos e imprimir el resultado
+'''
+
+# Ingreso de datos
+dato1 = input("Ingresar el primer numero : ")
+dato2 = input("Ingresar el segundo numero : ")
+
+# Tratamientos de datos
+
+# Funcion int() convierte un dato a entero, si es posible
+dato1 = int(dato1)
+dato2 = int(dato2)
+
+
+# Procesamiento de los datos
+resultado = dato1 + dato2
+
+# Resultado
+print("El resultado de la suma es :", resultado)
+
+
+
