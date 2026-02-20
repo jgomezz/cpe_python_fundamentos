1aa2ec755e45b0d6f6a4b21e922a71ced2d0a376
jgomezz <jgomezz@gmail.com>
2026-02-19 20:37:13 -0500

    update

--- sesion_01_ejercicios/06_ejercicio_input.py
+++ sesion_01_ejercicios/06_ejercicio_input.py
@@ -2,6 +2,7 @@
 Ingresar 2 numeros por teclado, sumarlos e imprimir el resultado
 '''
 
+'''
 # Ingreso de datos
 dato1 = input("Ingresar el primer numero : ")
 dato2 = input("Ingresar el segundo numero : ")
@@ -19,5 +20,18 @@ resultado = dato1 + dato2
 # Resultado
 print("El resultado de la suma es :", resultado)
 
+'''
 
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+num3 = int(input("Ingresa el tercer número: "))
+
+
+suma_inicial = num1 + num2
+
+
+resultado_final = suma_inicial - num3
+
+
+print(f"El resultado final es: {resultado_final}")
 
