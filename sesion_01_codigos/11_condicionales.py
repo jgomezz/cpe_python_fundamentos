5e24b434500d919d29e09c1453864bc644562642
jgomezz <jgomezz@gmail.com>
2026-02-19 21:47:26 -0500

    update

--- sesion_01_codigos/11_condicionales.py
+++ sesion_01_codigos/11_condicionales.py
@@ -2,8 +2,9 @@
 Curso : Programacion Basica de Python
 Sesion : 01 
 Tema : Introduccion a Python - Condicionales
-Fecha : 08/02/202x
+Fecha : 20/02/2026
 Author : Jaime Gomez
+Version : 1.2
 
 '''
 
@@ -17,7 +18,7 @@ Author : Jaime Gomez
  virtual de aprobacion.
 '''
 
-#'''
+'''
 
 NOTA_MINIMA = 11
 
@@ -32,8 +33,10 @@ if nota_final >= NOTA_MINIMA:
 #     Condicional : if ... else
 #####################################
 '''
- El estudiante aprueba con 11, en este caso se 
- le envia su constancia virtual. Si el estudiante 
+ Para que un estudiante pase un curso , 
+ debe aprobar con la nota minima de 11. 
+ , si aprueba se le envia su constancia
+ virtual de aprobacion. Si el estudiante 
  no aprueba solo se le envia su nota final.
 '''
 
@@ -41,7 +44,7 @@ if nota_final >= NOTA_MINIMA:
 
 NOTA_MINIMA = 11
 
-nota_final_estudiante  = 10 # nota final
+nota_final_estudiante  = 19 # nota final
 
 if nota_final_estudiante >= NOTA_MINIMA :
     print("Envio de constancia virtual")
@@ -62,11 +65,11 @@ else :
  tiene una nota menor a 10 solo se le envia su nota final.
 '''
 
-'''
+#'''
 NOTA_MINIMA_APROB = 11
 NOTA_MINIMA_RECUP = 10
 
-nota_final  = 13 # nota final
+nota_final  = 9.50 # nota final
 
 if nota_final >= NOTA_MINIMA_APROB :
     print("Envio de constancia virtual")
