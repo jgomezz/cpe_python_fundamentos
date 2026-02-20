d5d8c2eb633aedd6b7f9330a056927dfad157244
jgomezz <jgomezz@gmail.com>
2026-02-19 20:09:31 -0500

    update

--- sesion_01_ejercicios/05_ejercicio_type.py
+++ sesion_01_ejercicios/05_ejercicio_type.py
@@ -3,3 +3,15 @@ Generar 3 variables de tipo entero, boolean y cadena
 Usar la funcion type() para obtener el tipo de dato e imprimir
 el resultado por consola
 '''
+
+dia = 19 # Variable de tipo entero
+tipo_dato = type(dia)
+print("El tipo de dato de la variable dia es :", tipo_dato)
+
+es_estudiante = True # Variable de tipo boolean
+tipo_dato = type(es_estudiante)
+print("El tipo de dato de la variable es_estudiante es :", tipo_dato)
+
+nombre = "Jaime" # Variable de tipo cadena
+tipo_dato = type(nombre)
+print("El tipo de dato de la variable nombre es :", tipo_dato)
