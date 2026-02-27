611f445c0a75a9f513a01d36f51b6fa9b80975a5
jgomezz <jgomezz@gmail.com>
2026-02-26 20:35:32 -0500

    update

---
+++ sesion_02_ejercicios/03_ejercicios_listas.py
@@ -0,0 +1,10 @@
+
+'''
+Ejercicio : En la lista dada, 
+
+nros = [101,210,335,450,510,601,702]
+
+a) Mostrar todos los elementos de la lista a excepción del 210 y 510
+b) Mostrar los elementos de la lista que son mayores a 300
+c) Mostrar el promedio de los elementos de la lista
+'''
