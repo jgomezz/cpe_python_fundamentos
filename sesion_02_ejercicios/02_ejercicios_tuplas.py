77908a13a74eb6c98e24f85c519156430371b986
jgomezz <jgomezz@gmail.com>
2026-02-26 19:59:15 -0500

    update

---
+++ sesion_02_ejercicios/02_ejercicios_tuplas.py
@@ -0,0 +1,22 @@
+#             0     1      2
+nombres = ("Jose","Juan","pedro")
+
+print(nombres[0])
+print(nombres[1])
+
+
+'''
+Ejercicio : En la tupla dada, 
+
+mi_tupla = (10,20,30,40,50,60,70)
+
+a) mostrar los valores :
+  - (10,30,50)
+  - (50,)
+  - (40,50,60)
+  - (60, 70)
+
+b) recorrer la tupla usando un for
+   y mostrar todos los valores a exception del 40 y 70
+
+'''
