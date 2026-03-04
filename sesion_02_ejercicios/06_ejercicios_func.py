c947f83f6f5f3f254ef2ba1f67360f22a6a46881
jgomezz <jgomezz@gmail.com>
2026-03-03 20:23:13 -0500

    update

---
+++ sesion_02_ejercicios/06_ejercicios_func.py
@@ -0,0 +1,45 @@
+
+def f_suma(x,y):
+
+    s = x + y
+  
+    return s
+
+
+
+
+a = 11
+b = 2132
+suma  = a + b
+print(suma)
+
+suma  = f_suma(a,b)
+print(suma)
+
+a = 4
+b = 6
+#suma  = a + b
+suma  = f_suma(a,b)
+print(suma)
+
+
+
+a = 2
+b = 80
+#suma  = a + b
+suma  = f_suma(a,b)
+print(suma)
+
+
+'''
+Crear la funcion f_operacion que reciba 3 parametros, a, b y ope,
+dependiendo del valor de ope, se realizara la suma, resta, multiplicacion o division de a y b
+
+   f_operacion(3,5,'suma')
+   f_operacion(3,5,'resta')
+   f_operacion(3,5,'mult')
+   f_operacion(3,5,'div')
+
+
+'''
+
