667fc4db82fc307ce418102b6e104bf00634c963
jgomezz <jgomezz@gmail.com>
2026-03-03 19:50:48 -0500

    update

--- sesion_02_ejercicios/05_ejercicios_dic.py
+++ sesion_02_ejercicios/05_ejercicios_dic.py
@@ -10,9 +10,18 @@ tasas = {
     "2025" : 3.4,
     "2026" : 3.2
         }
-
+# 
 print(tasas["2024"])
 
-for anho, tasa in tasas.items():
-    print(f"En el año {anho} la tasa de crecimiento fue de {tasa}%")
+for key, value in tasas.items():
+    print(f"En el año {key} la tasa de crecimiento fue de {value}%")
 
+for key in tasas.keys():
+    print(f"En el año {key}")
+
+for key in tasas:
+    print(f"En el año {key}")
+
+
+for value in tasas.values():
+    print(f"La tasa de crecimiento fue de {value}%")
