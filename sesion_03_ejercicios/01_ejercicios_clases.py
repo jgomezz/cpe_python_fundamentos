7e82d8d4f723abd8145d4ebc6aa7d22d00607db2
jgomezz <jgomezz@gmail.com>
2026-03-03 21:46:31 -0500

    update

--- sesion_03_ejercicios/01_ejercicios_clases.py
+++ sesion_03_ejercicios/01_ejercicios_clases.py
@@ -39,3 +39,33 @@ doc_enrique = Docente(3, 'Enrique', 'López', 35) # Se instancia una clase
 print(doc_jaime.nombre)
 print(doc_gerardo.nombre)
 print(doc_enrique.nombre)
+
+
+print('----------- Tercera Version ------------------')
+
+class Docente:
+
+    # Constructor de la clase
+    def __init__(self, codigo, nombre, apellido, edad):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+
+    def obtener_info(self):
+        ret = f'Código: {self.codigo}, Nombre: {self.nombre} {self.apellido}, Edad: {self.edad}'
+        return ret
+    
+    def obtener_login(self):
+        ret = self.nombre[0].lower() + self.apellido.lower()
+        return ret
+
+
+doc_jaime = Docente(1, 'Jaime', 'García', 30) # Se instancia una clase
+doc_gerardo = Docente(2, 'Gerardo', 'Martínez', 25) # Se instancia una clase
+doc_enrique = Docente(3, 'Enrique', 'López', 35) # Se instancia una clase
+
+print(doc_jaime.obtener_info())
+print(doc_jaime.obtener_login())
+
--- sesion_03_ejercicios/README.md
+++ sesion_03_ejercicios/README.md
@@ -4,6 +4,7 @@
 
 
 ```python
+
 class Docente:
 
     # Constructor de la clase
@@ -14,13 +15,21 @@ class Docente:
         self.edad = edad
 
 
+    def obtener_info(self):
+        ret = f'Código: {self.codigo}, Nombre: {self.nombre} {self.apellido}, Edad: {self.edad}'
+        return ret
+    
+    def obtener_login(self):
+        ret = self.nombre[0].lower() + self.apellido.lower()
+        return ret
+
+
 doc_jaime = Docente(1, 'Jaime', 'García', 30) # Se instancia una clase
 doc_gerardo = Docente(2, 'Gerardo', 'Martínez', 25) # Se instancia una clase
 doc_enrique = Docente(3, 'Enrique', 'López', 35) # Se instancia una clase
 
-print(doc_jaime.nombre)
-print(doc_gerardo.nombre)
-print(doc_enrique.nombre)
+print(doc_jaime.obtener_info())
+print(doc_jaime.obtener_login())
 ```
 
 
