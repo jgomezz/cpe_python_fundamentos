8b264178b39916fc5a0a3650ee9f7279b1d096ae
jgomezz <jgomezz@gmail.com>
2026-03-03 21:19:55 -0500

    update

--- sesion_03_ejercicios/01_ejercicios_clases.py
+++ sesion_03_ejercicios/01_ejercicios_clases.py
@@ -1,4 +1,7 @@
 
+print('----------- Primera Version ------------------')
+
+
 '''
  Clase docente
 '''
@@ -17,6 +20,22 @@ print(doc_gerardo)
 print(doc_enrique)
 
 
+print('----------- Segunda Version ------------------')
+
+class Docente:
+
+    # Constructor de la clase
+    def __init__(self, codigo, nombre, apellido, edad):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
 
+doc_jaime = Docente(1, 'Jaime', 'García', 30) # Se instancia una clase
+doc_gerardo = Docente(2, 'Gerardo', 'Martínez', 25) # Se instancia una clase
+doc_enrique = Docente(3, 'Enrique', 'López', 35) # Se instancia una clase
 
+print(doc_jaime.nombre)
+print(doc_gerardo.nombre)
+print(doc_enrique.nombre)
--- sesion_03_ejercicios/README.md
+++ sesion_03_ejercicios/README.md
@@ -1,3 +1,9 @@
+## Introducción a Clases en Python
+
+<img src="uml.png"/>
+
+
+## Ejemplo de una tabla
 |Titulo|Descripcion|Observacion|
 |-|-|-|
 |Python|Lenguaje de Programacion|-|
