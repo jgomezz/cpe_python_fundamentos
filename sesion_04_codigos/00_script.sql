48321d5e6572d3d0dbf8861e68f750393d0f938c
jgomezz <jgomezz@gmail.com>
2026-03-10 21:40:08 -0500

    update

--- sesion_04_codigos/00_script.sql
+++ sesion_04_codigos/00_script.sql
@@ -1,5 +1,20 @@
 -- Crear la base de datos HR
-CREATE DATABASE HR;
+-- CREATE DATABASE HR;
 
 -- Borrar base de datos HR
-DROP DATABASE HR;
+-- DROP DATABASE HR;
+
+-- Seleccionar la base de datos HR
+USE HR;
+
+-- Crea la tabla EMPLEADOS
+CREATE TABLE EMPLEADOS (
+	NRO_EMP int(11) NOT NULL AUTO_INCREMENT,
+	NOMBRE varchar(50) NOT NULL,
+	APELLIDO varchar(50) NOT NULL,
+	SEXO enum('H','M') NOT NULL,
+	FECHA_NACIMIENTO date ,
+	FECHA_CONTRATO date ,
+	PRIMARY KEY (NRO_EMP)
+);
+
